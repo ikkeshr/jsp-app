e3bf502aca6f4f6616cc79e570d051846da4fa56
Ikkesh <ikesh.ramanna@gmail.com>
2021-01-20 23:03:46 +0400

    added navigation

--- src/main/java/practice/beans/HelloWorldBean.java
+++ src/main/java/practice/beans/HelloWorldBean.java
@@ -4,7 +4,7 @@ import javax.faces.bean.ManagedBean;
 
 @ManagedBean(name = "helloworld", eager = true)
 public class HelloWorldBean {
-
+	
 	public HelloWorldBean() {
 		System.out.println("Hello world web app practice started...");
 	}
