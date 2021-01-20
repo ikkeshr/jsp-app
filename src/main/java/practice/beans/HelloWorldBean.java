96fc19059140a362ce73ab48447441d0dbf0e6f2
Ikkesh <ikesh.ramanna@gmail.com>
2021-01-20 21:59:59 +0400

    init

---
+++ src/main/java/practice/beans/HelloWorldBean.java
@@ -0,0 +1,16 @@
+package practice.beans;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name = "helloworld", eager = true)
+public class HelloWorldBean {
+
+	public HelloWorldBean() {
+		System.out.println("Hello world web app practice started...");
+	}
+	
+	public String getMessage() {
+		return "Hello world from BEAN";
+	}
+	
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello World from practice project!</h2>
+</body>
+</html>
